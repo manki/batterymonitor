56201f94b134809ec5855de68714bda1b8a97053
mkannan <mkannan@google.com>
2015-08-13 22:08:10 -0700

    We have an alarm now, but the alarm does not tick, for whatever reason.

--- app/src/main/java/in/manki/batterymonitor/BatteryLevelTrackerService.java
+++ app/src/main/java/in/manki/batterymonitor/BatteryLevelTrackerService.java
@@ -1,6 +1,10 @@
 package in.manki.batterymonitor;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.BatteryManager;
@@ -10,35 +14,59 @@ import android.widget.Toast;
 
 public class BatteryLevelTrackerService extends Service {
     private static final String TAG = "BatteryLevelTrackingSvc";
+    private static final int NOW = 0;  // 0 milliseconds from now.
 
-    public BatteryLevelTrackerService() {
-    }
-
-    @Override
-    public IBinder onBind(Intent intent) {
-        // We don't provide binding. Return null.
-        return null;
-    }
+    private AlarmManager alarmManager;
+    private PendingIntent alarmIntent;
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         Toast.makeText(this, "Service starting", Toast.LENGTH_LONG).show();
-        Log.i(TAG, "onStartCommand " + getBatteryStatus());
+        Log.i(TAG, "Service starting");
+        if (alarmManager == null) {
+            alarmManager =
+                    (AlarmManager) getApplicationContext().getSystemService(Context.ALARM_SERVICE);
+        }
+        if (alarmIntent == null) {
+            Intent alarmReceiverIntent = new Intent(getApplicationContext(), AlarmReceiver.class);
+            alarmIntent = PendingIntent.getBroadcast(getApplicationContext(), 0, alarmReceiverIntent, 0);
+        }
+
+        long whenToStartTicking = NOW;
+        //~long interval = AlarmManager.INTERVAL_HOUR;
+        long interval = 2000;
+        assert alarmManager != null;
+        alarmManager.setRepeating(
+        //~alarmManager.setInexactRepeating(
+                AlarmManager.ELAPSED_REALTIME_WAKEUP, whenToStartTicking, interval, alarmIntent);
+
         return START_STICKY;
     }
 
-    private BatteryStatus getBatteryStatus() {
-        IntentFilter filter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
-        Intent batteryStatus = getApplicationContext().registerReceiver(null, filter);
-        int status = batteryStatus.getIntExtra(BatteryManager.EXTRA_STATUS, -1);
-        ChargeState chargeState = (status == BatteryManager.BATTERY_STATUS_CHARGING
-                || status == BatteryManager.BATTERY_STATUS_FULL) ? ChargeState.CHARGING : ChargeState.NOT_CHARGING;
+    private static class AlarmReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Toast.makeText(context, "Alarm ticking", Toast.LENGTH_LONG).show();
+            Log.i(TAG, "alarmTick " + getBatteryStatus(context));
+        }
 
-        int level = batteryStatus.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
-        int scale = batteryStatus.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
-        int percentage = (int) (100.0 * level / scale);
+        private BatteryStatus getBatteryStatus(Context context) {
+            IntentFilter filter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+            Intent batteryStatus = context.registerReceiver(null, filter);
 
-        return new BatteryStatus(chargeState, percentage);
+            assert batteryStatus != null;
+            int status = batteryStatus.getIntExtra(BatteryManager.EXTRA_STATUS, -1);
+            ChargeState chargeState =
+                    (status == BatteryManager.BATTERY_STATUS_CHARGING
+                            || status == BatteryManager.BATTERY_STATUS_FULL)
+                            ? ChargeState.CHARGING : ChargeState.NOT_CHARGING;
+
+            int level = batteryStatus.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
+            int scale = batteryStatus.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
+            int percentage = (int) (100.0 * level / scale);
+
+            return new BatteryStatus(chargeState, percentage);
+        }
     }
 
     private static final class BatteryStatus {
@@ -60,5 +88,11 @@ public class BatteryLevelTrackerService extends Service {
         CHARGING,
         NOT_CHARGING,
         ;
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // We don't provide binding. Return null.
+        return null;
     }
 }
